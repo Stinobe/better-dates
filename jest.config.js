8b46489c0bceccbf2aaf46bbbf2dbf35edcbc0f1
Stino.be <stijn@vanwijmeersch.com>
2019-11-23 11:02:26 +0100

    :wrench: Change configuration for test & pkg info

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   coveragePathIgnorePatterns: ["/node_modules/", "/lib/", "/tests/"],
+  testPathIgnorePatterns: ["/node_modules/", "/.history/"],
   testRegex: "(/tests/.*|(\\.|/)(test|spec))\\.[jt]sx?$"
 };
