036eb088eb141105f2e0c16efbac08b28e54de48
Stino.be <stijn@vanwijmeersch.com>
2019-12-07 21:17:47 +0100

    Add coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   coveragePathIgnorePatterns: ["/node_modules/", "/dist/", "/tests/"],
   testPathIgnorePatterns: ["/node_modules/", "/.history/"],
-  testRegex: "(/tests/.*|(\\.|/)(test|spec))\\.[jt]sx?$"
+  testRegex: "(/tests/.*|(\\.|/)(test|spec))\\.[jt]sx?$",
+  coverageThreshold: {
+    "global": {
+      "branches": 80,
+      "functions": 80,
+      "lines": 80,
+      "statements": 0
+    }
+  }
 };
